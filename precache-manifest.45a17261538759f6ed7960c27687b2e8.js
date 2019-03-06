5d401ac4e172b02627cf22df20cac8a4d7a3fd37
chi-liang ju <pk0204dg888@gmail.com>
2019-03-06 15:29:32 +1000

    Updates

---
+++ precache-manifest.45a17261538759f6ed7960c27687b2e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "785b1e29d9f442f12c09",
+    "url": "/Contactmanagers/static/js/runtime~main.785b1e29.js"
+  },
+  {
+    "revision": "105dacc8676237f16d8c",
+    "url": "/Contactmanagers/static/js/main.105dacc8.chunk.js"
+  },
+  {
+    "revision": "a2d9523be0a69c84ab57",
+    "url": "/Contactmanagers/static/js/2.a2d9523b.chunk.js"
+  },
+  {
+    "revision": "105dacc8676237f16d8c",
+    "url": "/Contactmanagers/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "a2d9523be0a69c84ab57",
+    "url": "/Contactmanagers/static/css/2.854b2da5.chunk.css"
+  },
+  {
+    "revision": "44ca39b12d631c2819f70afc247e1fe9",
+    "url": "/Contactmanagers/index.html"
+  }
+];
